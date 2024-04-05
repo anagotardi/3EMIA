260daf533082b8aa45299844d02b9c583b16d674
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 16:09:06 -0300

    acReact

---
+++ ac2React/acinw2/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Header from './componentes/Header/Header'
+import Carrossel from './componentes/Carrossel/Carrossel'
+import QuemSomos from './componentes/QuemSomos/QuemSomos'
+import Habilidades from './componentes/Habilidades/Habilidades'
+import Formulario from './componentes/Formulario/Formulario'
+import Footer from './componentes/Footer/Footer'
+
+function App(){
+
+  return(
+<>
+  <Header/>
+  <Carrossel/>
+  <QuemSomos/>
+  <Habilidades/>
+  <Formulario/>
+  <Footer/>
+  </>
+  )
+  
+}
+
+
+export default App
---
+++ ac2React/acinw2/src/componentes/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div>
+            <p class="text-center">Telefone: (11)99999-9999. Instagram: fevasgo. Tik Tok: fevasgo. Facebook: fevasgo</p>
+        </div>
+    )
+}
+
+export default Footer
