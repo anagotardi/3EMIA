62424e93172f865261c601d20035351d2194f0de
Ana Gotardi <99182974+anagotardi@users.noreply.github.com>
2024-04-05 16:39:01 -0300

    Delete ac2React/acinw2 directory

--- ac2React/acinw2/src/componentes/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <div>
-            <p class="text-center">Telefone: (11)99999-9999. Instagram: fevasgo. Tik Tok: fevasgo. Facebook: fevasgo</p>
-        </div>
-    )
-}
-
-export default Footer
--- ac2React/acinw2/src/componentes/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-function Header(){
-    return(
-        <h1 className="text-center">Bem-vindo ao Fevasgo!</h1>
-    )
-   
-}
-
-export default Header
